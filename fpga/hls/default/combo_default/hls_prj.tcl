e057d6a6fabe1ba0879a0d9545cf5ee993eda800
bingleilou <loubinglei@outlook.com>
2023-02-02 12:56:34 +1100

    modify the ap_fixed<64,32) to ap_fixed<32,8>

--- fpga/hls/default/combo_default/hls_prj.tcl
+++ fpga/hls/default/combo_default/hls_prj.tcl
@@ -6,6 +6,7 @@ open_solution -reset "solution-rfsoc"
 set_part {xczu28dr-ffvg1517-2-e}
     
 create_clock -period 5 -name default
+config_export -format ip_catalog -rtl verilog
 csynth_design
 export_design -format ip_catalog
 exit
